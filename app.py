eedce2ac84c9c4f85d3b8e572f7d797b0891f223
Vinicius <59659214+ZeAndradee@users.noreply.github.com>
2025-10-06 11:33:42 -0300

    Implement user authentication and management features with Flask blueprints

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
 from flask import Flask, render_template 
-
+from routes.auth import auth_bp
+from routes.users import users_bp
 app = Flask(__name__)
 
-users = [
-{"id":1, "nome":"Jose",    "email": "jose@gmail.com","perfil":"Coordenador", "status":"ativo"},
- {"id":2, "nome": "Maria",  "email": "mariazinha@gmail.com","perfil": "Supervizor", "status":"inativo"},
- {"id":3, "nome": "Carla",  "email": "carlinha@gmail.com","perfil": "Desenvolvedor", "status":"ativo"},
- {"id":4, "nome": "Pablo",  "email": "pablinho@gmail.com","perfil": "Analista de Dados", "status":"inativo"},
- {"id":5, "nome": "Wesley", "email": "wesley@gmail.com","perfil": "Designer", "status":"ativo"},
- {"id":6, "nome": "Amina",  "email": "Amina@gmail.com","perfil": "QA", "status":"ativo"}]
 
 
-@app.route("/")
-def home():
-    return render_template("home.html")
+app.register_blueprint(auth_bp)
+app.register_blueprint(users_bp)
 
-@app.route("/login")
-def login():
-    return render_template("lauth/login.html")
+# @app.route("/")
+# def home():
+#     return render_template("home.html")
+
+# # @app.route("/login")
+# # def login():
+# #     return render_template("lauth/login.html")
+
+# @app.route("/show")
+# def show():
+#     return render_template("users/show.html", users=users)
